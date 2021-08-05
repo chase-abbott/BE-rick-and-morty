fbd75071a5097116c691486f078e92b78923d97f
Chase <cabbott93@gmail.com>
2021-08-05 16:02:37 -0700

    passing login test w/o password encryption

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -4,18 +4,33 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('user auth routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
+  const user = {
+    username: 'chase',
+    password: 'password'
+  };
   it('tests a signup route', async () => {
-    const user = {
-      username: 'chase',
-      password: 'password'
-    };
     const { body } = await request(app)
       .post('/api/auth/signup')
       .send(user);
 
     expect(body).toEqual({ username: 'chase', userId: '1' });
+  });
+
+  it('logs a user in who has already signed up', async () => {
+    const { body } = await request(app)
+      .post('/api/auth/login')
+      .send(user);
+    
+    expect(body).toEqual({ username: 'chase', userId: '1' });
+
+    const newReq = await request(app)
+      .post('/api/auth/login')
+      .send({ username: 'chase', password: 'p' });
+
+    expect(newReq.text).toEqual('Invalid Username or Password');
+
   });
 });
